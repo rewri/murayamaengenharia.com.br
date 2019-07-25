c6972c39505d2e1acf3d15e488064f1ddaa06432
Rérisson Fumes <rkfumes@gmail.com>
2019-07-24 22:33:34 -0300

    Ajustes finais para https

--- webroot/js/custom.js
+++ webroot/js/custom.js
@@ -1,78 +1,94 @@
 $(document).ready(function() {
-
     // NAVBAR
-    $(document).on('scroll', function() {
-        if ($(this).scrollTop() >= $('#after-carousel').position().top + 80) {
-            $('.navbar').addClass('solid');
-            $('.main-logo').addClass('solid-logo');
+    $(document).on("scroll", function() {
+        if ($(this).scrollTop() >= $("#after-carousel").position().top + 80) {
+            $(".navbar").addClass("solid");
+            $(".main-logo").addClass("solid-logo");
         } else {
-            $('.navbar').removeClass('solid');
-            $('.main-logo').removeClass('solid-logo');
+            $(".navbar").removeClass("solid");
+            $(".main-logo").removeClass("solid-logo");
         }
     });
 
     // NAVBAR
     function initNavbar() {
-
         $("#navbar-home").click(function() {
-            $([document.documentElement, document.body]).animate({
-                scrollTop: $("#topbar").offset().top
-            }, 1200);
+            $([document.documentElement, document.body]).animate(
+                {
+                    scrollTop: $("#topbar").offset().top
+                },
+                1200
+            );
         });
 
-
         $("#navbar-who").click(function() {
-            $([document.documentElement, document.body]).animate({
-                scrollTop: $("#who").offset().top - 30
-            }, 1200);
+            $([document.documentElement, document.body]).animate(
+                {
+                    scrollTop: $("#who").offset().top - 30
+                },
+                1200
+            );
         });
 
         $("#navbar-projects").click(function() {
-            $([document.documentElement, document.body]).animate({
-                scrollTop: $("#projects").offset().top - 120
-            }, 1200);
+            $([document.documentElement, document.body]).animate(
+                {
+                    scrollTop: $("#projects").offset().top - 120
+                },
+                1200
+            );
         });
 
         $("#navbar-services").click(function() {
-            $([document.documentElement, document.body]).animate({
-                scrollTop: $("#project_types").offset().top - 120
-            }, 1200);
+            $([document.documentElement, document.body]).animate(
+                {
+                    scrollTop: $("#project_types").offset().top - 120
+                },
+                1200
+            );
         });
 
         $("#navbar-budget, #carousel-budget").click(function() {
-            $([document.documentElement, document.body]).animate({
-                scrollTop: $("#budget").offset().top - 70
-            }, 1200);
+            $([document.documentElement, document.body]).animate(
+                {
+                    scrollTop: $("#budget").offset().top - 70
+                },
+                1200
+            );
         });
 
         $("#navbar-contributors").click(function() {
-            $([document.documentElement, document.body]).animate({
-                scrollTop: $("#contributors").offset().top - 30
-            }, 1200);
+            $([document.documentElement, document.body]).animate(
+                {
+                    scrollTop: $("#contributors").offset().top - 30
+                },
+                1200
+            );
         });
     }
 
-    $(function(){
+    $(function() {
         var navMain = $(".navbar-collapse");
-        navMain.on("click", "a:not([data-toggle])", null, function () {
-            navMain.collapse('hide');
+        navMain.on("click", "a:not([data-toggle])", null, function() {
+            navMain.collapse("hide");
         });
     });
 
     // WhatsApp
     function initWhatsAppButton() {
-        const whatsBtn = $('#whatsButton');
+        const whatsBtn = $("#whatsButton");
         whatsBtn.hide();
         whatsBtn.floatingWhatsApp({
-            phone: '5514996635051 ',
-            popupMessage: 'Olá, como podemos ajudar?',
+            phone: "5514996635051 ",
+            popupMessage: "Olá, como podemos ajudar?",
             message: "Olá, gostaria de solicitar um orçamento.",
             showPopup: true,
             showOnIE: false,
-            headerTitle: 'Murayama Engenharia',
-            headerColor: '#128C7E',
-            backgroundColor: '#128C7E',
-            buttonImage: '<img src="http://murayamaengenharia.com.br/webroot/img/whatsapp.svg" alt="WhatsApp" />'
+            headerTitle: "Murayama Engenharia",
+            headerColor: "#128C7E",
+            backgroundColor: "#128C7E",
+            buttonImage:
+                '<img src="//murayamaengenharia.com.br/webroot/img/whatsapp.svg" alt="WhatsApp" />'
         });
 
         $(document).scroll(function() {
@@ -87,7 +103,7 @@ $(document).ready(function() {
 
     // Social icons
     function initSocialIcons() {
-        const socialIconDiv = $('#social-side-links');
+        const socialIconDiv = $("#social-side-links");
         socialIconDiv.hide();
         $(document).scroll(function() {
             var y = $(this).scrollTop();
@@ -97,11 +113,9 @@ $(document).ready(function() {
                 socialIconDiv.fadeOut();
             }
         });
-
     }
 
     initNavbar();
     initWhatsAppButton();
     initSocialIcons();
 });
-
